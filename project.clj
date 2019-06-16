e023303a5ac174605449e5da03653728af624f5d
Andrew Lai <andrew.s.lai5@gmail.com>
2019-06-16 08:23:47 -0500

    Upgraded react versions

--- project.clj
+++ project.clj
@@ -2,29 +2,30 @@
   :description "Template for full stack development in Clojure"
   :dependencies [[cheshire "5.8.1"]
                  [clj-http "3.9.1"]
-                 [cljs-http "0.1.46"]
-                 [cljs-ajax "0.7.5"]
-                 [cljsjs/react "15.2.1-1"]
-                 [cljsjs/react-dom "15.2.1-1"]
                  [coreagile/defenv "1.0.2"]
                  [hiccup "1.0.5"]
                  [http-kit "2.3.0"]
                  [metosin/compojure-api "1.1.12"]
                  [org.clojure/clojure "1.9.0" :upgrade false]
-                 [org.clojure/clojurescript "1.10.520"]
                  [org.clojure/core.async "0.3.442"
                   :exclusions [org.clojure/tools.reader]]
                  [org.clojure/java.jdbc "0.6.1"]
                  [org.postgresql/postgresql "9.4-1201-jdbc41"]
+
+
+                 [cljs-http "0.1.46"]
+                 [cljs-ajax "0.7.5"]
+                 [cljsjs/react "16.8.6-0"]
+                 [cljsjs/react-dom "16.8.6-0"]
+                 [org.clojure/clojurescript "1.10.520"]
+                 [binaryage/devtools "0.9.10"]
+                 [clj-commons/secretary "1.2.4"]
+                 [day8.re-frame/tracing "0.5.1"]
                  [reagent "0.8.1"]
-                 [reagent "0.6.0-rc"] ;; REQUIRED FOR DEV PROFILE
                  [re-frame "0.10.5"]
                  [ring "1.7.1"]
                  [sablono "0.7.4"]
-
-                 [binaryage/devtools "0.9.10"]
-                 [clj-commons/secretary "1.2.4"]
-                 [day8.re-frame/tracing "0.5.1"]]
+                 ]
 
   :plugins [[lein-figwheel "0.5.18"]
             [lein-cljsbuild "1.1.7"]]
