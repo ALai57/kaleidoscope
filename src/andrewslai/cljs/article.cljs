949bc288d0f769b742df382e9497fb768db289ff
Andrew Lai <andrew.s.lai5@gmail.com>
2019-11-06 19:59:02 -0600

    Parse style tag so that it can be rendered in React

--- src/andrewslai/cljs/article.cljs
+++ src/andrewslai/cljs/article.cljs
@@ -16,6 +16,42 @@
 (defn get-content [active-content]
   (first (get-in active-content [:article :content])))
 
+
+(defn string->tokens
+  "Takes a string with syles and parses it into properties and value tokens"
+  [style]
+  {:pre [(string? style)]
+   :post [(even? (count %))]}
+  (->> (clojure.string/split style #";")
+       (mapcat #(clojure.string/split % #":"))
+       (map clojure.string/trim)))
+
+(defn tokens->map
+  "Takes a seq of tokens with the properties (even) and their values (odd)
+   and returns a map of {properties values}"
+  [tokens]
+  {:pre [(even? (count tokens))]
+   :post [(map? %)]}
+  (zipmap (keep-indexed #(if (even? %1) %2) tokens)
+          (keep-indexed #(if (odd? %1) %2) tokens)))
+
+(defn style->map
+  "Takes an inline style attribute stirng and converts it to a React Style map"
+  [style]
+  (if (empty? style)
+    style
+    (tokens->map (string->tokens style))))
+
+(defn hiccup->sablono
+  "Transforms a style inline attribute into a style map for React"
+  [coll]
+  (clojure.walk/postwalk
+   (fn [x]
+     (if (map? x)
+       (update-in x [:style] style->map)
+       x))
+   coll))
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Formatting title, JS and content
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -33,7 +69,11 @@
 (defn format-content [{:keys [content]}]
   [:div#article-content
    (when content
-     (first (map h/as-hiccup (h/parse-fragment content))))])
+     (->> content
+          h/parse-fragment
+          (map h/as-hiccup)
+          first
+          hiccup->sablono))])
 
 (defn insert-dynamic-js! [{:keys [dynamicjs]}]
   (map format-js dynamicjs))
@@ -55,4 +95,8 @@
   (map h/as-hiccup (h/parse-fragment (:content (get-content (:active-content @app-db)))))
   (format-js "test-paragraph.js")
   (get-js (:active-content @app-db))
+  )
+
+(comment
+  (hiccup->sablono (first (map h/as-hiccup (h/parse-fragment "<div><font color=\"#ce181e\"><font face=\"Ubuntu Mono\"><font size=\"5\"><b>A basic test</b></font></font></font><p style=\"color:red\">Many of the</p><p>Usually, that database</p></div>"))))
   )
