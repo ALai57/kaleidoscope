b655ec6889f745a8f0ed5ca3bf1fad14fe35b801
Andrew Lai <andrew.s.lai5@gmail.com>
2020-03-23 22:01:24 -0500

    WIP: Started adding tests for other handler routes. Failing bc of env vars

--- test/andrewslai/clj/handler_test.clj
+++ test/andrewslai/clj/handler_test.clj
@@ -1,8 +1,9 @@
 (ns andrewslai.clj.handler-test
   (:require [andrewslai.clj.handler :as h]
+            [andrewslai.clj.persistence.mock :as mock-db]
+            [andrewslai.clj.utils :refer [parse-response-body]]
             [clojure.test :refer [deftest testing is]]
-            [ring.mock.request :as mock]
-            [andrewslai.clj.utils :refer [parse-response-body]]))
+            [ring.mock.request :as mock]))
 
 (deftest ping-test
   (testing "Ping works properly"
@@ -11,9 +12,25 @@
                                        parse-response-body
                                        keys
                                        set)))))
+
 (deftest home-test
-  (testing "Index page works properly")
-  (is (= 200 (-> (mock/request :get "/")
-                 h/app
-                 :status))))
+  (testing "Index page works properly"
+    (is (= 200 (-> (mock/request :get "/")
+                   h/app
+                   :status)))))
+
+;; Failing because it uses the wrong environment variables when testing
+;;  look into mounting components...
+(deftest get-full-article-test
+  (let [captured-input (atom [])]
+    (testing "Can retrieve a full article"
+      (with-redefs
+        [mock-db/get-full-article
+         (fn [article-name]
+           (println "HELLO!!!!")
+           (swap! captured-input conj article-name))]
+        (let [{:keys [status body] :as response}
+              (h/app (mock/request :get "/get-article/thoughts/test-article"))]
+          (is (= 200 status))
+          (is (= nil @captured-input)))))))
 
