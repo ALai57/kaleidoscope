36152b13948e7b3380aa4cfb37ed7576e35de66e
Andrew Lai <andrew.s.lai5@gmail.com>
2020-04-06 09:55:10 -0500

    Add logging test

--- test/andrewslai/clj/handler_test.clj
+++ test/andrewslai/clj/handler_test.clj
@@ -22,15 +22,31 @@
       (is (= 200 status)))))
 
 ;; TODO: Add tests that logging works properly
+
+(defn captured-logging [logging-atom]
+  {:level :debug
+   :appenders {:println {:enabled? true,
+                         :level :debug
+                         :output-fn
+                         (fn [data] (force (:msg_ data)))
+                         :fn (fn [data]
+                               (let [{:keys [output_]} data]
+                                 (swap! logging-atom conj (force output_))))}}})
+
 (deftest logging-test
   (testing "Logging works properly"
-    (let [{:keys [status body]}
-          ((h/wrap-middleware h/bare-app {:logging timbre/*config*})
+    (let [logging-atom (atom [])
+          {:keys [status body]}
+          ((h/wrap-middleware h/bare-app {:logging (captured-logging logging-atom)})
            (mock/request :get "/ping"))]
-      (is (= 200 status))
-      (is (= #{:sha :service-status} (-> body
-                                         body->map
-                                         keys
-                                         set))))))
+      (is (= 1 (count @logging-atom))))))
 
-;;(logging-test)
+#_(logging-test)
+
+#_(let [logging-atom (atom [])]
+    (timbre/with-config (captured-logging logging-atom)
+      (timbre/info "Yo"))
+    @logging-atom)
+
+#_(println (:appenders timbre/*config*))
+
