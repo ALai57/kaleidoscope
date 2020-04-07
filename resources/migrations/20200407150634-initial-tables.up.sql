93a5ab80764f8ef81db987e201b7f5df52dc15b5
Andrew Lai <andrew.s.lai5@gmail.com>
2020-04-07 14:30:55 -0500

    More modifications to initial migrations

--- resources/migrations/20200407150634-initial-tables.up.sql
+++ resources/migrations/20200407150634-initial-tables.up.sql
@@ -59,3 +59,67 @@ INSERT INTO permissions (title, description, active) VALUES
 INSERT INTO roles (title, description, active) VALUES
        ('admin', 'Has all administrator privileges', true),
        ('read_only', 'Does not have admin privileges', true);
+
+--;;
+
+CREATE TABLE organizations(
+       id INT,
+       name text,
+       url text,
+       image_url text,
+       description text
+);
+
+--;;
+
+ALTER TABLE organizations ADD CONSTRAINT unique_organizations UNIQUE (name);
+
+--;;
+
+CREATE TABLE IF NOT EXISTS projects(
+       id INT,
+       name text,
+       url text,
+       image_url text,
+       description text,
+       organization_names text[],
+       skills_names JSONB[]
+);
+
+--;;
+
+ALTER TABLE projects ADD CONSTRAINT unique_projects UNIQUE (name);
+
+--;;
+
+CREATE TABLE IF NOT EXISTS skills(
+       id INT,
+       name text,
+       url text,
+       image_url text,
+       description text,
+       skill_category text
+);
+
+--;;
+
+ALTER TABLE skills ADD CONSTRAINT unique_skills UNIQUE (name);
+
+--;;
+
+CREATE TABLE articles(
+       title VARCHAR (100),
+       article_tags VARCHAR (32),
+       timestamp TIMESTAMP,
+       author VARCHAR (50),
+       article_url VARCHAR (100),
+       article_id SERIAL PRIMARY KEY
+);
+
+--;;
+
+CREATE TABLE content(
+       article_id INT,
+       content text,
+       dynamicjs text[]
+);
