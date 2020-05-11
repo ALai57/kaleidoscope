4bfbd95c1005bffa30ca4fc1ef818a14dfb735da
Andrew Lai <andrew.s.lai5@gmail.com>
2020-05-10 20:42:06 -0500

    Clean DB ns

--- src/andrewslai/cljs/db.cljs
+++ src/andrewslai/cljs/db.cljs
@@ -8,9 +8,9 @@
 (s/def ::active-panel keyword?)
 (s/def ::loading boolean?)
 (s/def ::db
-  (s/keys :rEeq-un [::active-panel
-                    ::active-content
-                    ::loading?]))
+  (s/keys :req-un [::active-panel
+                   ::active-content
+                   ::loading?]))
 
 ;; -- Default app-db Value  ---------------------------------------------------
 
@@ -23,22 +23,5 @@
    :recent-content nil
    :loading? false
    :loading-resume? false
-   :circles [{:name "circle 1"
-              :x 10
-              :y 10
-              :r 10
-              :color "black"}
-             {:name "circle 2"
-              :x 35
-              :y 35
-              :r 15
-              :color "red"}
-             {:name "circle 3"
-              :x 100
-              :y 100
-              :r 30
-              :color "blue"}]
-   :active-user nil
-   :avatar nil
    :editor-data nil
    :editor-metadata {}})
