dbad6796cb0f3cf2690538af305c2700380fdb3f
ALai57 <andrew.s.lai5@gmail.com>
2020-09-17 23:20:50 -0500

    Extract duplicated create! functionality to single function

--- src/andrewslai/clj/persistence/users2.clj
+++ src/andrewslai/clj/persistence/users2.clj
@@ -6,19 +6,39 @@
             [honeysql.helpers :as hh]
             [slingshot.slingshot :refer [throw+ try+]]))
 
+;; CREATE looks similar for all different ways of creating
+;; So does delete
+;; Can these be combined?
+;;
+;; If so, should I just provide functions that call a generic "create" "delete", etc?
+
+;; TODO: Deal with m being a collection or not
+(defn create! [database table m & {:keys [ex-subtype
+                                          validation-spec]}]
+  (when validation-spec
+    (validate validation-spec m :IllegalArgumentException))
+  (try+
+   (p/transact! database (-> (hh/insert-into table)
+                             (hh/values [m])))
+   (catch org.postgresql.util.PSQLException e
+     (throw+ (merge {:type :PersistenceException
+                     :message {:data (select-keys m [:username :email])
+                               :reason (.getMessage e)}}
+                    (when ex-subtype
+                      {:subtype ex-subtype}))))))
+
 (defrecord UserDB [database]
   User
   (create-user! [this user]
-    (try+
-     (validate :andrewslai.user/user user :IllegalArgumentException)
-     (p/transact! database (-> (hh/insert-into :users)
-                               (hh/values [user])))
-     (catch org.postgresql.util.PSQLException e
-       (throw+ {:type :PersistenceException
-                :subtype ::UnableToCreateUser
-                :message {:data (select-keys user [:username :email])
-                          :reason (.getMessage e)
-                          :feedback "Try a different username and/or email"}}))))
+    (create! database
+             :users user
+             :ex-subtype :UnableToCreateUser
+             :validation-spec :andrewslai.user/user))
+
+  (get-users [this]
+    (p/select database {:select [:*]
+                        :from [:users]}))
+
   (create-login! [this id password]
     (validate :andrewslai.user/password password :IllegalArgumentException)
     (try+
@@ -31,13 +51,9 @@
        (throw+ {:type :PersistenceException
                 :subtype ::UnableToCreateLogin
                 :message {:data {:id id}
-                          :reason (.getMessage e)}}))))
-  (get-users [this]
-    (p/select database {:select [:*]
-                        :from [:users]})))
+                          :reason (.getMessage e)}})))))
 
 (comment
-  (require '[andrewslai.clj.env :as env])
   (require '[andrewslai.clj.dev-tools :as tools])
 
   (def example-user
