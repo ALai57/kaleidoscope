edea991ebe235b80278fa8d6bda294e85c08c8a8
ALai57 <andrew.s.lai5@gmail.com>
2020-10-27 23:04:01 -0500

    Minor formatting, renaming, etc

--- resources/migrations/20200407235822-seed-resume-info.up.sql
+++ resources/migrations/20200407235822-seed-resume-info.up.sql
@@ -1,21 +1,21 @@
 
 INSERT INTO organizations VALUES
-       (1, 'HELIX', 'https://helix.northwestern.edu', 'images/nu-helix-logo.svg', 'Northwestern science outreach magazine'),
-       (2, 'YMCA', 'http://www.mcgawymca.org/', 'images/ymca-logo.svg', 'YMCA'),
-       (3, 'VAI', 'www.hnvi.org', 'images/vai-logo.svg', 'Vietnamese Association of Illinois'),
-       (4, 'ChiPy', 'https://www.chipy.org', 'images/chipy-logo.svg', 'Chicago Python User Group'),
-       (5, 'ChiHackNight', 'https://chihacknight.org/', 'images/chi-hack-night-logo.svg', 'Chicago Civic Hacking'),
-       (6, 'Center for Leadership', 'https://lead.northwestern.edu/leadership/index.html', 'images/center-for-leadership-logo.svg', 'Northwestern Center for Leadership'),
-       (7, 'MGLC', 'https://www.mccormick.northwestern.edu/graduate-leadership-council/', 'images/mglc-logo.svg', 'McCormick Graduate Leadership Council (Engineering Graduate school leadership)');
+(1, 'HELIX', 'https://helix.northwestern.edu', 'images/nu-helix-logo.svg', 'Northwestern science outreach magazine'),
+(2, 'YMCA', 'http://www.mcgawymca.org/', 'images/ymca-logo.svg', 'YMCA'),
+(3, 'VAI', 'www.hnvi.org', 'images/vai-logo.svg', 'Vietnamese Association of Illinois'),
+(4, 'ChiPy', 'https://www.chipy.org', 'images/chipy-logo.svg', 'Chicago Python User Group'),
+(5, 'ChiHackNight', 'https://chihacknight.org/', 'images/chi-hack-night-logo.svg', 'Chicago Civic Hacking'),
+(6, 'Center for Leadership', 'https://lead.northwestern.edu/leadership/index.html', 'images/center-for-leadership-logo.svg', 'Northwestern Center for Leadership'),
+(7, 'MGLC', 'https://www.mccormick.northwestern.edu/graduate-leadership-council/', 'images/mglc-logo.svg', 'McCormick Graduate Leadership Council (Engineering Graduate school leadership)');
 
 --;;
 
 
 INSERT INTO projects VALUES
-       (1, 'The experiment that shocked the world', 'https://helix.northwestern.edu/article/experiment-shocked-world', 'images/nu-helix-logo.svg', 'A short article on Luigi Galvani and his discovery of animal electricity.', '{HELIX}', array['{"Heap Analytics": "Did something with Heap"}'::json]),
-       (2, 'Project SOAR', 'http://www.mcgawymca.org/youth-teens/after-school-care-activities/mentoring/project-soar-news/', 'images/ymca-logo.svg', 'Youth mentoring through the YMCA', '{YMCA}', array['{"Sumologic": "Did something with Sumologic"}'::json]),
-       (3, 'Teaching ESL and Citizenship', 'www.hnvi.org', 'images/vai-logo.svg', 'Teaching conversational English and preparation for the citizenship exam.', '{VAI}', array['{"Pandas": "Did something with Pandas"}'::json]),
-       (4, 'ChiPy Mentorship Program', 'https://chipymentor.org', 'images/chipy-logo.svg', 'Mentee in a Python mentorship program.', '{ChiPy}', array['{"Pandas": "Did something with Pandas"}'::json]);
+(1, 'The experiment that shocked the world', 'https://helix.northwestern.edu/article/experiment-shocked-world', 'images/nu-helix-logo.svg', 'A short article on Luigi Galvani and his discovery of animal electricity.', '{HELIX}', array['{"Heap Analytics": "Did something with Heap"}'::json]),
+(2, 'Project SOAR', 'http://www.mcgawymca.org/youth-teens/after-school-care-activities/mentoring/project-soar-news/', 'images/ymca-logo.svg', 'Youth mentoring through the YMCA', '{YMCA}', array['{"Sumologic": "Did something with Sumologic"}'::json]),
+(3, 'Teaching ESL and Citizenship', 'www.hnvi.org', 'images/vai-logo.svg', 'Teaching conversational English and preparation for the citizenship exam.', '{VAI}', array['{"Pandas": "Did something with Pandas"}'::json]),
+(4, 'ChiPy Mentorship Program', 'https://chipymentor.org', 'images/chipy-logo.svg', 'Mentee in a Python mentorship program.', '{ChiPy}', array['{"Pandas": "Did something with Pandas"}'::json]);
 
 --;;
 
--- scripts/deployment/copy-to-s3.sh
+++ scripts/deployment/copy-to-s3.sh
@@ -3,17 +3,20 @@ function copy-to-s3 {
     local SHA=`git rev-parse --short HEAD`
 
     # Zip up the files to be included in deployment
-    zip --exclude '*.git*' \
-        --exclude '*node_modules/*' \
-        --exclude '*.elasticbeanstalk*' \
-        --exclude '*deployment*.zip' \
-        --exclude 'iac/*' \
-        --exclude 'target/andrewslai-0.0.1.jar' \
-        --exclude 'target/classes/*' \
-        --exclude 'target/stale/*' \
+    #
+    zip --include '/target/andrewslai.jar' 'resources/public/*' \
         -r deployment-$SHA.zip .
+    #zip --exclude '*.git*' \
+        #--exclude '*node_modules/*' \
+        #--exclude '*.elasticbeanstalk*' \
+        #--exclude '*deployment*.zip' \
+        #--exclude 'iac/*' \
+        #--exclude 'target/andrewslai-0.0.1.jar' \
+        #--exclude 'target/classes/*' \
+        #--exclude 'target/stale/*' \
+        #-r deployment-$SHA.zip .
 
     # Copy zip file to S3
-    aws s3 mb s3://andrewslai-eb --region us-east-1
-    aws s3 cp deployment-$SHA.zip s3://andrewslai-eb --region us-east-1
+    aws s3 mb s3://andrewslai-artifacts --region us-east-1
+    aws s3 cp deployment-$SHA.zip s3://andrewslai-artifacts --region us-east-1
 }
