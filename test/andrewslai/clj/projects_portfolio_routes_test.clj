2f228e1cae97b93ef8bcebc557f56134dc9a13dd
ALai57 <andrew.s.lai5@gmail.com>
2021-01-15 20:47:31 -0600

    Update projects_portfolio tests

--- test/andrewslai/clj/projects_portfolio_routes_test.clj
+++ test/andrewslai/clj/projects_portfolio_routes_test.clj
@@ -1,13 +1,12 @@
 (ns andrewslai.clj.projects-portfolio-routes-test
-  (:require [andrewslai.clj.persistence.postgres-test :as ptest]
-            [andrewslai.clj.entities.portfolio :as portfolio]
-            [andrewslai.clj.test-utils :as tu :refer [defdbtest]]
-            [andrewslai.clj.utils :refer [parse-body]]
+  (:require [andrewslai.clj.embedded-postgres :refer [with-embedded-postgres]]
+            [andrewslai.clj.test-utils :as tu]
             [clojure.spec.alpha :as s]
-            [clojure.test :refer [is testing]]))
+            [clojure.test :refer [deftest is]]
+            [matcher-combinators.test]))
 
-(defdbtest resume-info-test ptest/db-spec
-  (testing "GET endpoint returns portfolio"
-    (let [response (tu/get-request "/projects-portfolio")]
-      (is (= 200 (:status response)))
-      (is (s/valid? :andrewslai.portfolio/portfolio (parse-body response))))))
+(deftest resume-info-test
+  (with-embedded-postgres database
+    (is (match? {:status 200
+                 :body #(s/valid? :andrewslai.portfolio/portfolio %)}
+                (tu/http-request :get "/projects-portfolio" {:database database})))))
