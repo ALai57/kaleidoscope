0973e170901d07e8aee3f9b43c1de75d51f707b0
ALai57 <andrew.s.lai5@gmail.com>
2021-03-20 21:30:13 -0500

    Version 0.0.11

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject andrewslai "0.0.11-SNAPSHOT"
+(defproject andrewslai "0.0.11"
   :description "Template for full stack development in Clojure"
   :dependencies [[aleph "0.4.7-alpha7"]
                  [buddy/buddy-auth "2.2.0"]
