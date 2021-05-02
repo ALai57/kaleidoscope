1bc8e6891f0116d8170989f8b9bb05f54d89fdd6
ALai57 <andrew.s.lai5@gmail.com>
2021-05-01 20:28:13 -0500

    Clean up Terraform backends

--- iac/wedding-bucket/backend.tf
+++ iac/wedding-bucket/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket     = "andrewslai-tf"
-    key        = "ecs-keycloak/terraform.tfstate"
+    key        = "wedding-bucket/terraform.tfstate"
     region     = "us-east-1"
   }
 }
---
+++ iac/wedding-bucket/s3.tf
@@ -0,0 +1,3 @@
+resource "aws_s3_bucket" "andrewslai_wedding" {
+  bucket = "andrewslai-wedding"
+}
