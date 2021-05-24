39510d2ce67749ce674d0b19da42da4d36109902
ALai57 <andrew.s.lai5@gmail.com>
2021-05-23 22:07:29 -0500

    Version 0.0.31-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject andrewslai "0.0.30"
+(defproject andrewslai "0.0.31-SNAPSHOT"
   :description "Template for full stack development in Clojure"
   :dependencies [[aleph "0.4.7-alpha7"]
                  [amazonica "0.3.156"]
