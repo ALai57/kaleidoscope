5e4d71ead59edd29e30f8ddc661d4e2cbadb414d
ALai57 <andrew.s.lai5@gmail.com>
2021-06-03 22:51:57 -0500

    Cleanup static-content test

--- test/andrewslai/clj/static_content_test.clj
+++ test/andrewslai/clj/static_content_test.clj
@@ -22,14 +22,6 @@
         tmpfile (tu/mktmp "delete.txt" tmpdir)
         path    (str (.getName tmpdir) "/" (.getName tmpfile))]
 
-    (is (nil? (.getResource (.getContextClassLoader (Thread/currentThread)) path)))
-    (is (some? (.getResource tu/tmp-loader path)))))
-
-(deftest resources-from-classpath-test
-  (let [tmpdir  (tu/mktmpdir "andrewslai-test")
-        tmpfile (tu/mktmp "delete.txt" tmpdir)
-        path    (str (.getName tmpdir) "/" (.getName tmpfile))]
-
     (are [pred loader]
       (pred (response/resource-response path {:loader loader}))
 
@@ -48,82 +40,26 @@
       map? (.getName tmpfile) {:root (.getAbsolutePath tmpdir)})))
 
 (deftest s3-response-test
-  (let [bucket "andrewslai-wedding"
+  (let [bucket   "andrewslai-wedding"
         endpoint "media/"]
     (sandbox/with (comp (sandbox/just
                          (s3/list-objects-v2
                           ([req]
-                           (or (is (match? {:prefix endpoint
+                           (or (is (match? {:prefix      endpoint
                                             :bucket-name bucket}
                                            req))
                                (throw (Exception. "Invalid inputs")))
-                           {:bucket-name bucket
+                           {:bucket-name     bucket
                             :common-prefixes []
-                            :key-count 1}
+                            :key-count       1}
                            )))
                         sandbox/always-fail)
       (are [expected loader]
         (is (match? expected (response/resource-response endpoint
                                                          {:loader loader})))
 
-        nil? (.getContextClassLoader (Thread/currentThread))
+        nil?                   (.getContextClassLoader (Thread/currentThread))
         {:status 200 :body ()} (-> {:bucket bucket
                                     :creds  {:profile "none"}}
                                    s3-storage/map->S3
                                    s3p/s3-loader)))))
-
-(comment
-  (def tmpdir
-    (System/getProperty "java.io.tmpdir"))
-
-  (defn pwd
-    []
-    (System/getProperty "user.dir"))
-
-  (let [tmpdir  (tu/mktmpdir "andrewslai-test")
-        tmpfile (tu/mktmp "delete.txt" tmpdir)
-        path    (str (.getName tmpdir) "/" (.getName tmpfile))]
-
-    (is (nil? (.getResource (.getContextClassLoader (Thread/currentThread)) path)))
-    (is (some? (.getResource tu/tmp-loader path))))
-
-
-
-  (let [bucket "andrewslai-wedding"
-        endpoint "media/"]
-    (sandbox/with (comp (sandbox/just
-                         (s3/list-objects-v2
-                          ([req]
-                           (println req)
-                           (or (is (match? {:bucket-name bucket
-                                            :prefix endpoint}
-                                           req))
-                               (throw (Exception. "Invalid inputs")))
-
-                           (println "RETURN")
-
-                           {:common-prefixes [{:prefix "x/"}]
-                            :contents {:e-tag "w"
-                                       :key "x"
-                                       :last-modified (java.time.Instant/now)
-                                       :owner {:display-name "hello"
-                                               :id "something"}
-                                       :size  10
-                                       :storage-class "something"}
-                            :delimiter "hello"
-                            :encoding-type "utf-8"
-                            :is-truncated false
-                            :key-count 1
-                            :max-keys 10
-                            :name bucket
-                            :prefix "hello"}
-                           #_{:content-type "something"
-                              :response-metadata {:request-id "he"}}
-                           #_{:content-type "something"
-                              :response-metadata {:request-id "he"}})))
-                        sandbox/always-nothing)
-      (response/resource-response (format "s3p:/%s/%s" bucket endpoint)
-                                  {:loader (s3p/s3-loader)})))
-
-
-  )
