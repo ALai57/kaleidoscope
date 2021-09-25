0296fc16bdbd5d1a15c7ca82a3440391be026b94
ALai57 <andrew.s.lai5@gmail.com>
2021-09-25 17:17:52 -0500

    Remove handler ns

--- src/andrewslai/clj/main.clj
+++ src/andrewslai/clj/main.clj
@@ -1,7 +1,11 @@
 (ns andrewslai.clj.main
   (:gen-class)
-  (:require [andrewslai.clj.handler :as h]
+  (:require [aleph.http :as http]
             [andrewslai.clj.config :as cfg]
+            [andrewslai.clj.handler :as h]
+            [andrewslai.clj.routes.andrewslai :as andrewslai]
+            [andrewslai.clj.routes.wedding :as wedding]
+            [andrewslai.clj.virtual-hosting :as vh]
             [taoensso.timbre :as log]
             [taoensso.timbre.appenders.core :as appenders]))
 
@@ -10,6 +14,19 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (log/merge-config!
  {:appenders {:spit (appenders/spit-appender {:fname "log.txt"})}})
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Running the server
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defn start-app
+  [{:keys [port andrewslai wedding] :as configuration}]
+  (http/start-server
+   (vh/host-based-routing
+    {#"caheriaguilar.and.andrewslai.com" {:priority 0
+                                          :app      (wedding/wedding-app wedding)}
+     #".*"                               {:priority 100
+                                          :app      (andrewslai/andrewslai-app andrewslai)}})
+   {:port port}))
 
 (defn -main
   "Start a server and run the application"
