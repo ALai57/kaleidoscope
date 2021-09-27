76f8079f0b0e3eaa73490494e810fb304987c549
ALai57 <andrew.s.lai5@gmail.com>
2021-09-26 21:16:38 -0500

    Remove `figwheel_backend` ns

--- src/andrewslai/clj/utils.clj
+++ src/andrewslai/clj/utils.clj
@@ -4,18 +4,6 @@
             [clojure.spec.alpha :as s]
             [slingshot.slingshot :refer [throw+]]))
 
-(defn parse-body [ring-map]
-  (-> ring-map
-      :body
-      slurp
-      (json/parse-string keyword)))
-
-(defn file->bytes [file]
-  (with-open [xin (clojure.java.io/input-stream file)
-              xout (java.io.ByteArrayOutputStream.)]
-    (clojure.java.io/copy xin xout)
-    (.toByteArray xout)))
-
 (defn validate [type data ex]
   (if (s/valid? type data)
     true
@@ -27,16 +15,6 @@
                   :reason reason
                   :feedback (or (:feedback data)
                                 reason)}}))))
-
-(defn string->bytes [s]
-  (->> s
-       (map (comp byte int))
-       byte-array))
-
-(defn b64-encode [s]
-  (->> s
-       b64/encode
-       String.))
 
 (defn deep-merge
   [m1 m2]
