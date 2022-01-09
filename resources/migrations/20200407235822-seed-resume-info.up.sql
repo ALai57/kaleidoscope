908579863ede0cd1bd879b0dee03ac1a93fa43a0
Andrew Lai <alai@splashfinancial.com>
2022-01-09 17:59:51 -0600

    Modifying DB schemas to accomodate H2

--- resources/migrations/20200407235822-seed-resume-info.up.sql
+++ resources/migrations/20200407235822-seed-resume-info.up.sql
@@ -8,17 +8,8 @@ INSERT INTO organizations VALUES
 (6, 'Center for Leadership', 'https://lead.northwestern.edu/leadership/index.html', 'images/center-for-leadership-logo.svg', 'Northwestern Center for Leadership'),
 (7, 'MGLC', 'https://www.mccormick.northwestern.edu/graduate-leadership-council/', 'images/mglc-logo.svg', 'McCormick Graduate Leadership Council (Engineering Graduate school leadership)');
 
---;;
-
-
-INSERT INTO projects VALUES
-(1, 'The experiment that shocked the world', 'https://helix.northwestern.edu/article/experiment-shocked-world', 'images/nu-helix-logo.svg', 'A short article on Luigi Galvani and his discovery of animal electricity.', '{HELIX}', array['{"Heap Analytics": "Did something with Heap"}'::json]),
-(2, 'Project SOAR', 'http://www.mcgawymca.org/youth-teens/after-school-care-activities/mentoring/project-soar-news/', 'images/ymca-logo.svg', 'Youth mentoring through the YMCA', '{YMCA}', array['{"Sumologic": "Did something with Sumologic"}'::json]),
-(3, 'Teaching ESL and Citizenship', 'www.hnvi.org', 'images/vai-logo.svg', 'Teaching conversational English and preparation for the citizenship exam.', '{VAI}', array['{"Pandas": "Did something with Pandas"}'::json]),
-(4, 'ChiPy Mentorship Program', 'https://chipymentor.org', 'images/chipy-logo.svg', 'Mentee in a Python mentorship program.', '{ChiPy}', array['{"Pandas": "Did something with Pandas"}'::json]);
 
 --;;
-
 
 INSERT INTO skills VALUES
 (1, 'Periscope Data', '', 'images/periscope-logo.svg', 'Analytics tool for dashboarding and data visualization....', 'Analytics Tools'),
@@ -26,3 +17,32 @@ INSERT INTO skills VALUES
 (3, 'Heap Analytics', '', 'images/heap-logo.svg', 'Analytics tool for collecting/analyzing user behavior data and funnel conversion.', 'Analytics Tools'),
 (4, 'Jupyter Notebooks', '', 'images/jupyter-logo.svg', 'Analytics tool for data analytics/data science.', 'Analytics Tools'),
 (5, 'Pandas', '', 'images/pandas-logo.svg', 'Python package for manipulating data.', 'Analytics Tools');
+
+--;;
+
+
+INSERT INTO projects VALUES
+(1, 'The experiment that shocked the world', 'https://helix.northwestern.edu/article/experiment-shocked-world', 'images/nu-helix-logo.svg', 'A short article on Luigi Galvani and his discovery of animal electricity.'),
+(2, 'Project SOAR', 'http://www.mcgawymca.org/youth-teens/after-school-care-activities/mentoring/project-soar-news/', 'images/ymca-logo.svg', 'Youth mentoring through the YMCA'),
+(3, 'Teaching ESL and Citizenship', 'www.hnvi.org', 'images/vai-logo.svg', 'Teaching conversational English and preparation for the citizenship exam.'),
+(4, 'ChiPy Mentorship Program', 'https://chipymentor.org', 'images/chipy-logo.svg', 'Mentee in a Python mentorship program.');
+
+
+--;;
+
+
+INSERT INTO projects_organizations VALUES
+(1, 1),
+(2, 2),
+(3, 3),
+(4, 4);
+
+
+--;;
+
+
+INSERT INTO projects_skills VALUES
+(1, 3, 'For writing `the experiment that shocked the world`, I did something with Heap'),
+(2, 2, 'At project SOAR, I did something with Sumologic'),
+(3, 5, 'At VAI, I did something with Pandas'),
+(4, 5, 'At Chipy, I did something with Pandas');
