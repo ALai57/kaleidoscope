2f77f7bc7c3af0c6f2062368b544c6e814fadf6a
Andrew Lai <alai@splashfinancial.com>
2022-01-22 12:45:33 -0600

    version

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (def COMPILE-TIME-LOG-LEVEL
   :error)
 
-(defproject org.clojars.alai57/andrewslai "0.0.49"
+(defproject org.clojars.alai57/andrewslai "0.0.50-SNAPSHOT"
   :url "https://github.com/ALai57/andrewslai"
   :license {:name         "Eclipse Public License - v 1.0"
             :url          "http://www.eclipse.org/legal/epl-v10.html"
