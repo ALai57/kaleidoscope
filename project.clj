db91a1ec4b481823481f5c8739d552bccd95d165
Andrew Lai <andrew.s.lai5@gmail.com>
2022-06-26 23:02:22 -0500

    Version 0.0.60

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (def COMPILE-TIME-LOG-LEVEL
   :info)
 
-(defproject org.clojars.alai57/andrewslai "0.0.60-SNAPSHOT"
+(defproject org.clojars.alai57/andrewslai "0.0.60"
   :url "https://github.com/ALai57/andrewslai"
   :license {:name         "Eclipse Public License - v 1.0"
             :url          "http://www.eclipse.org/legal/epl-v10.html"
