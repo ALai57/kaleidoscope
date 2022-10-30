615aeb421933fd4e54a38de15640d63e819e1341
Andrew Lai <alai@splashfinancial.com>
2022-10-29 21:01:11 -0500

    Add article and branch created at

--- resources/migrations/20200407150634-initial-tables.up.sql
+++ resources/migrations/20200407150634-initial-tables.up.sql
@@ -86,10 +86,14 @@ SELECT
     ab.id AS branch_id,
     ab.branch_name,
     ab.published_at,
+    ab.created_at AS branch_created_at,
+    ab.modified_at AS branch_modified_at,
     a.id AS article_id,
     a.author,
     a.article_url,
-    a.article_tags
+    a.article_tags,
+    a.created_at AS article_created_at,
+    a.modified_at AS article_modified_at
 FROM article_versions av
      JOIN article_branches ab ON ab.id = av.branch_id
      JOIN articles a ON a.id = ab.article_id
@@ -101,10 +105,14 @@ SELECT
     ab.id AS branch_id,
     ab.branch_name,
     ab.published_at,
+    ab.created_at,
+    ab.modified_at,
     a.id AS article_id,
     a.author,
     a.article_url,
-    a.article_tags
+    a.article_tags,
+    a.created_at AS article_created_at,
+    a.modified_at AS article_modified_at
 FROM article_branches ab
      JOIN articles a ON a.id = ab.article_id
 
