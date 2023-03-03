ee35f181fdd1df4fef83da2100915d08ed740889
Andrew Lai <andrew.s.lai5@gmail.com>
2023-03-02 20:05:23 -0600

    Prototype client for KC user API

--- src/andrewslai/clj/http_api/auth/keycloak.clj
+++ src/andrewslai/clj/http_api/auth/keycloak.clj
@@ -1,8 +1,10 @@
 (ns andrewslai.clj.http-api.auth.keycloak
   (:require [andrewslai.clj.http-api.auth.jwt :as jwt]
+            [clj-http.client :as http]
             [taoensso.timbre :as log])
   (:import org.keycloak.adapters.KeycloakDeploymentBuilder
            org.keycloak.adapters.rotation.AdapterTokenVerifier
+           (org.keycloak.admin.client Keycloak)
            org.keycloak.jose.jws.JWSInput
            org.keycloak.representations.adapters.config.AdapterConfig))
 
@@ -44,6 +46,98 @@
         (catch Exception e
           (log/error "Authentication exception" (pr-str e)))))))
 
+;; Set up a Keycloak serice user
+(comment
+  ;; Setting up a service user in Keycloak.
+  ;;
+  ;; First, create a new client. This will be the Java client that will be able
+  ;; to talk to Keycloak.
+  ;;
+  ;; After creating the client, download the client credentials/secret
+  ;; information. This will be used to authenticate. Use the
+  ;; `Keycloak/getInstance` builder to create a new Keycloak client.
+
+
+  ;; Setting up roles in Keycloak
+  ;;
+  ;; Then, on the `Role Mapping` screen, assign roles to that user.  That user
+  ;; will need roles from the `relam-management` client to be able to view and
+  ;; manipulate users.
+
+  (def kc-service-user
+    (Keycloak/getInstance "http://localhost:8080"
+                          "test"
+                          "local-svc-user" ;; username
+                          "local-svc-user" ;; pw
+                          "test-login-java"
+                          "DES2AG0AjGggQKe1L25VPduVVHF5oaP0"))
+
+  (defn get-token
+    [kc-instance]
+    (:token (bean (.getAccessToken (.tokenManager kc-instance)))))
+
+  (defn get-users
+    [kc-instance]
+    (:body (http/request {:method  :get
+                          :url     "http://localhost:8080/admin/realms/test/users"
+                          :headers {"Authorization" (format "Bearer %s" (get-token kc-instance))}
+                          :as      :json})))
+
+  (defn create-user!
+    [kc-instance user]
+    (:body (http/request {:method  :post
+                          :url     "http://localhost:8080/admin/realms/test/users"
+                          :headers {"Authorization" (format "Bearer %s" (get-token kc-instance))}
+                          :body    {:email "my"}
+                          :as      :json})))
+
+  (def x
+    (get-users kc-service-user))
+  x;; => [{:requiredActions [],
+  ;;      :email "something@a.com",
+  ;;      :username "local-svc-user",
+  ;;      :disableableCredentialTypes [],
+  ;;      :firstName "",
+  ;;      :emailVerified false,
+  ;;      :id "aed3e21b-484d-40a7-8459-2e6fd6e60653",
+  ;;      :lastName "",
+  ;;      :notBefore 0,
+  ;;      :totp false,
+  ;;      :access {:manageGroupMembership true,
+  ;;               :view true,
+  ;;               :mapRoles true,
+  ;;               :impersonate false,
+  ;;               :manage true},
+  ;;      :enabled true,
+  ;;      :createdTimestamp 1677806861965} ]
+
+  (defn verified-user?
+    [user]
+    (:emailVerified user))
+
+  (defn email
+    [user]
+    (:email user))
+
+  (defn id
+    [user]
+    (:id user))
+
+  (defn username
+    [user]
+    (:username user))
+
+  (defn first-name
+    [user]
+    (:firstName user))
+
+  (defn last-name
+    [user]
+    (:lastName user))
+
+  (map (juxt id email verified-user?) x)
+  )
+
 (comment
 
   (def token
@@ -53,7 +147,7 @@
 
   ((make-keycloak-authenticator {:realm             "test"
                                  :ssl-required      "external"
-                                 :auth-server-url   "http://172.17.0.1:8080/auth/"
+                                 :auth-server-url   "http://172.17.0.1:8080"
                                  :client-id         "test-login-java"
                                  :client-secret     "18c28e7a-3eb6-4726-b8c7-9c5d02f6bc88"
                                  :confidential-port 0})
