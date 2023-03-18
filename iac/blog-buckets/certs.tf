8542c4f9544e628f6a71878fc442877980805c7a
Andrew Lai <andrew.s.lai5@gmail.com>
2023-03-17 23:48:31 -0500

    Refactored Certs

--- iac/blog-buckets/certs.tf
+++ iac/blog-buckets/certs.tf
@@ -5,36 +5,19 @@
 # Load Balancer
 ##############################################################
 data "aws_alb" "main" {
-  name   = "andrewslai-production"
+  name = "andrewslai-production"
+}
+
+data "aws_lb_listener" "main" {
+  load_balancer_arn = data.aws_alb.main.arn
+  port              = 443
 }
 
 # Created this manually in AWS console - go to AWS Certificate Manager
 # to request a cert. Then create DNS records using the AWS console.
-data "aws_acm_certificate" "issued" {
-  domain   = "caheriaguilar.com"
-  statuses = ["ISSUED"]
-}
-
-data "aws_lb_listener" "main" {
-  load_balancer_arn = "${data.aws_alb.main.arn}"
-  port              = 443
-}
-
-# Use existing target group to point traffic to existing web server
-data "aws_alb_target_group" "main" {
-  name = "andrewslai-production"
-}
-
-# Add SSL Cert for caheriaguilar.com to existing LB
-# Manually added an A record to Route53 to redirect traffic to ELB
-resource "aws_lb_listener_certificate" "caheriaguilar_cert" {
-  listener_arn    = "${data.aws_lb_listener.main.arn}"
-  certificate_arn = "${data.aws_acm_certificate.issued.arn}"
-}
-
-
-data "aws_acm_certificate" "sahiltalkingcents" {
-  domain   = "sahiltalkingcents.com"
+data "aws_acm_certificate" "blog_certs" {
+  for_each = toset(["caheriaguilar.com", "sahiltalkingcents.com"])
+  domain   = each.key
   statuses = ["ISSUED"]
 }
 
@@ -42,7 +25,8 @@ data "aws_acm_certificate" "sahiltalkingcents" {
 # Go to AWS Certificate Manager. Request new cert for site.
 # Then go to Route53 and add an A record that redirects to the Load balancer
 # Manually added an A record to Route53 to redirect traffic to ELB
-resource "aws_lb_listener_certificate" "sahiltalkingcents_cert" {
-  listener_arn    = "${data.aws_lb_listener.main.arn}"
-  certificate_arn = "${data.aws_acm_certificate.sahiltalkingcents.arn}"
+resource "aws_lb_listener_certificate" "listener_certs" {
+  for_each = data.aws_acm_certificate.blog_certs
+  listener_arn    = data.aws_lb_listener.main.arn
+  certificate_arn = each.value.arn
 }
