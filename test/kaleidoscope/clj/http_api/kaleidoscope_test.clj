086ffc83a48b9c63b5de389c5dabef8bef4f057b
Andrew Lai <andrew.s.lai5@gmail.com>
2023-03-21 22:59:01 -0500

    Merge wedding and kaleidoscope handlers

--- test/kaleidoscope/clj/http_api/kaleidoscope_test.clj
+++ test/kaleidoscope/clj/http_api/kaleidoscope_test.clj
@@ -1,15 +1,16 @@
 (ns kaleidoscope.clj.http-api.kaleidoscope-test
-  (:require [kaleidoscope.clj.api.portfolio :as portfolio]
-            [kaleidoscope.clj.http-api.kaleidoscope :as kaleidoscope]
+  (:require [clojure.java.io :as io]
+            [clojure.spec.alpha :as s]
+            [clojure.test :refer [are deftest is testing use-fixtures]]
+            [kaleidoscope.clj.api.portfolio :as portfolio]
             [kaleidoscope.clj.http-api.cache-control :as cc]
+            [kaleidoscope.clj.http-api.kaleidoscope :as kaleidoscope]
             [kaleidoscope.clj.init.env :as env]
             [kaleidoscope.clj.persistence.filesystem :as fs]
             [kaleidoscope.clj.test-main :as tm]
             [kaleidoscope.clj.test-utils :as tu]
             [kaleidoscope.clj.utils.core :as util]
-            [clojure.java.io :as io]
-            [clojure.spec.alpha :as s]
-            [clojure.test :refer [are deftest is testing use-fixtures]]
+            [kaleidoscope.cljc.specs.albums :refer [example-album example-album-2]]
             [matcher-combinators.matchers :as m]
             [matcher-combinators.test]
             [ring.mock.request :as mock]
@@ -124,6 +125,9 @@
 
     "GET `/admin` is not publicly accessible"
     {:status 401} (mock/request :get "/admin")
+
+    "POST `/media/` is not publicly accessible"
+    {:status 401} (mock/request :post "/media/")
 
     "GET `/projects-portfolio` is publicly accessible"
     {:status 200} (mock/request :get "/projects-portfolio")
@@ -540,3 +544,190 @@
                         (app (-> (mock/request :get "https://andrewslai.com/groups")
                                  (mock/header "Authorization" "Bearer user first-user")))))
             ))))))
+
+(deftest index.html-test
+  (let [system (->> {"KALEIDOSCOPE_DB_TYPE"                     "embedded-h2"
+                     "KALEIDOSCOPE_WEDDING_AUTH_TYPE"           "custom-authenticated-user"
+                     "KALEIDOSCOPE_WEDDING_AUTHORIZATION_TYPE"  "use-access-control-list"
+                     "KALEIDOSCOPE_WEDDING_STATIC_CONTENT_TYPE" "in-memory"}
+                    (env/start-system! env/WEDDING-BOOT-INSTRUCTIONS)
+                    env/prepare-wedding)
+        app    (->> system
+                    kaleidoscope/kaleidoscope-app
+                    tu/wrap-clojure-response)]
+    (is (match? {:status  200
+                 :headers {"Cache-Control" cc/revalidate-0s}
+                 :body    [{:name "afile"} {:name "adir" :type "directory"}]}
+                (app {:request-method :get
+                      :headers        {"Authorization" "Bearer x"}
+                      :uri            "/media/"})))))
+
+(deftest albums-test
+  (let [app (->> {"KALEIDOSCOPE_DB_TYPE"                     "embedded-h2"
+                  "KALEIDOSCOPE_WEDDING_AUTH_TYPE"           "always-unauthenticated"
+                  "KALEIDOSCOPE_WEDDING_AUTHORIZATION_TYPE"  "public-access"
+                  "KALEIDOSCOPE_WEDDING_STATIC_CONTENT_TYPE" "none"}
+                 (env/start-system! env/WEDDING-BOOT-INSTRUCTIONS)
+                 env/prepare-wedding
+                 kaleidoscope/kaleidoscope-app
+                 tu/wrap-clojure-response)]
+    (testing "No albums in DB to start"
+      (is (match? {:status 200 :body (comp (partial = 3) count)}
+                  (app (mock/request :get "https://andrewslai.com/albums")))))
+
+    (let [{:keys [body] :as result} (app (-> (mock/request :post "https://andrewslai.com/albums")
+                                             (mock/json-body example-album)))]
+
+      (testing "Create new album"
+        (is (match? {:status 200 :body map?}
+                    result)))
+
+      (testing "Retrieve newly created album"
+        (is (match? {:status 200 :body (-> example-album
+                                           (assoc :modified-at string?
+                                                  :created-at  string?)
+                                           (update :cover-photo-id str))}
+                    (app (mock/request :get (format "https://andrewslai.com/albums/%s" (:id body)))))))
+
+      (testing "Update album"
+        (is (match? {:status 200 :body {:id (:id body)}}
+                    (-> (mock/request :put (format "https://andrewslai.com/albums/%s" (:id body)))
+                        (mock/json-body {:album-name "Updated name"})
+                        app)))
+        (is (match? {:status 200 :body {:album-name "Updated name"}}
+                    (app (mock/request :get (format "https://andrewslai.com/albums/%s" (:id body))))))))))
+
+(defn string-uuid?
+  [s]
+  (and (string? s)
+       (uuid? (java.util.UUID/fromString s))))
+
+(deftest album-contents-test
+  (let [app (->> {"KALEIDOSCOPE_DB_TYPE"                     "embedded-h2"
+                  "KALEIDOSCOPE_WEDDING_AUTH_TYPE"           "always-unauthenticated"
+                  "KALEIDOSCOPE_WEDDING_AUTHORIZATION_TYPE"  "public-access"
+                  "KALEIDOSCOPE_WEDDING_STATIC_CONTENT_TYPE" "in-memory"}
+                 (env/start-system! env/WEDDING-BOOT-INSTRUCTIONS)
+                 env/prepare-wedding
+                 kaleidoscope/kaleidoscope-app
+                 tu/wrap-clojure-response)]
+    (let [photo-upload-result (:body (app (make-example-file-upload-request "https://andrewslai.com" "lock.svg")))
+          album-create-result (:body (app (-> (mock/request :post "https://andrewslai.com/albums")
+                                              (mock/json-body example-album))))
+          photo-id            (:id photo-upload-result)
+          album-id            (:id album-create-result)]
+
+      (testing "Album is empty to start"
+        (is (match? {:status 200 :body []}
+                    (app (-> (mock/request :get (format "https://andrewslai.com/albums/%s/contents" album-id)))))))
+
+      (let [result           (app (-> (mock/request :post (format "https://andrewslai.com/albums/%s/contents" album-id))
+                                      (mock/json-body [{:id photo-id}])))
+            album-content-id (get-in result [:body 0 :id])]
+        (testing "Successfully added photo to album"
+          (is (match? {:status 200 :body [{:id string-uuid?}]}
+                      result))
+          (is (match? {:status 200 :body [{:album-id         album-id
+                                           :photo-id         photo-id
+                                           :album-content-id album-content-id}]}
+                      (app (-> (mock/request :get (format "https://andrewslai.com/albums/%s/contents" album-id))))))
+          (is (match? {:status 200 :body {:album-id         album-id
+                                          :photo-id         photo-id
+                                          :album-content-id album-content-id}}
+                      (app (-> (mock/request :get (format "https://andrewslai.com/albums/%s/contents/%s" album-id album-content-id)))))))
+
+        (let [delete-result (app (mock/request :delete (format "https://andrewslai.com/albums/%s/contents/%s" album-id album-content-id)))]
+          (testing "Successfully removed photo from album"
+            (is (match? {:status 204}
+                        delete-result))
+            (is (match? {:status 200 :body empty?}
+                        (app (mock/request :get (format "https://andrewslai.com/albums/%s/contents" album-id)))))
+            (is (match? {:status 404}
+                        (app (mock/request :get (format "https://andrewslai.com/albums/%s/contents/%s" album-id album-content-id))))))
+          )
+        ))))
+
+(deftest contents-retrieval-test
+  (let [app (->> {"KALEIDOSCOPE_DB_TYPE"                     "embedded-h2"
+                  "KALEIDOSCOPE_WEDDING_AUTH_TYPE"           "always-unauthenticated"
+                  "KALEIDOSCOPE_WEDDING_AUTHORIZATION_TYPE"  "public-access"
+                  "KALEIDOSCOPE_WEDDING_STATIC_CONTENT_TYPE" "in-memory"}
+                 (env/start-system! env/WEDDING-BOOT-INSTRUCTIONS)
+                 env/prepare-wedding
+                 kaleidoscope/kaleidoscope-app
+                 tu/wrap-clojure-response)]
+
+    ;; Add a photo to two separate albums
+    (let [{photo-1-id :id} (:body (app (make-example-file-upload-request "https://andrewslai.com" "foo.svg")))
+          {photo-2-id :id} (:body (app (make-example-file-upload-request "https://andrewslai.com" "bar.svg")))
+          {album-1-id :id} (:body (app (-> (mock/request :post "https://andrewslai.com/albums")
+                                           (mock/json-body example-album))))
+          {album-2-id :id} (:body (app (-> (mock/request :post "https://andrewslai.com/albums")
+                                           (mock/json-body example-album-2))))]
+
+      (testing "Contents are empty to start"
+        (is (match? {:status 200 :body []}
+                    (app (-> (mock/request :get "https://andrewslai.com/albums/-/contents"))))))
+
+      (app (-> (mock/request :post (format "https://andrewslai.com/albums/%s/contents" album-1-id))
+               (mock/json-body [{:id photo-1-id}
+                                {:id photo-2-id}])))
+      (app (-> (mock/request :post (format "https://andrewslai.com/albums/%s/contents" album-2-id))
+               (mock/json-body [{:id photo-1-id}])))
+
+      (testing "Contents retrieved from multiple albums"
+        (is (match? {:status 200 :body [{:album-name (:album-name example-album)
+                                         :photo-id   photo-1-id}
+                                        {:album-name (:album-name example-album)
+                                         :photo-id   photo-2-id}
+                                        {:album-name (:album-name example-album-2)
+                                         :photo-id   photo-1-id}]}
+                    (app (-> (mock/request :get "https://andrewslai.com/albums/-/contents"))))) ))))
+
+(deftest albums-auth-test
+  (let [app (->> {"KALEIDOSCOPE_DB_TYPE"                     "embedded-h2"
+                  "KALEIDOSCOPE_WEDDING_AUTH_TYPE"           "custom-authenticated-user"
+                  "KALEIDOSCOPE_WEDDING_AUTHORIZATION_TYPE"  "use-access-control-list"
+                  "KALEIDOSCOPE_WEDDING_STATIC_CONTENT_TYPE" "in-memory"}
+                 (env/start-system! env/WEDDING-BOOT-INSTRUCTIONS)
+                 env/prepare-wedding
+                 kaleidoscope/kaleidoscope-app)]
+    (testing "Default access rules restrict access"
+      (is (match? {:status 401}
+                  (app (mock/request :get "https://andrewslai.com/albums")))))))
+
+(comment
+  (require '[clj-http.client :as http])
+
+  (println (:body (tu/assemble-multipart "OZqYohSB93zIWImnnfy2ekkaK8I_BDbVmtiTi"
+                                         [{:part-name    "file-contents"
+                                           :file-name    "lock.svg"
+                                           :content-type "image/svg+xml"
+                                           :content      (-> "public/images/lock.svg"
+                                                             clojure.java.io/resource
+                                                             slurp)}
+                                          {:part-name    "file-contents"
+                                           :file-name    "lock.svg"
+                                           :content-type "image/svg+xml"
+                                           :content      (-> "public/images/lock.svg"
+                                                             clojure.java.io/resource
+                                                             slurp)}])))
+
+  ;; 2021-09-04: This is working - need to make sure the actual index.html can
+  ;; make the same request
+  (http/request {:scheme      "http"
+                 :server-name "caheriaguilar.and.andrewslai.com.localhost"
+                 :server-port "5000"
+                 :method      :put
+                 :uri         "/media/lock.svg"
+                 :multipart   [{:name "name" :content "lock.svg"}
+                               {:name "content-type" :content "image/svg+xml"}
+                               {:name "foo.txt" :part-name "eggplant" :content "Eggplants"}
+                               {:name    "lock.svg"
+                                :content (-> "public/images/lock.svg"
+                                             clojure.java.io/resource
+                                             clojure.java.io/input-stream)}]})
+
+
+
+  )
