c2ef6042a7d586f4fcf50f5421b30297f15f117a
Andrew Lai <andrew.s.lai5@gmail.com>
2023-03-25 16:40:39 -0500

    Get all frontend app JS from Kaleidoscope bucket

--- src/kaleidoscope/clj/http_api/kaleidoscope.clj
+++ src/kaleidoscope/clj/http_api/kaleidoscope.clj
@@ -63,11 +63,39 @@
   (tap> "Stuff")
   )
 
+(defn get-resource
+  [static-content-adapters {:keys [uri headers] :as request}]
+  (let [result (-> static-content-adapters
+                   (get (bucket-name request))
+                   (fs/get uri (if-let [version (get-in request [:headers "if-none-match"])]
+                                 {:version version}
+                                 {})))]
+    (cond
+      (fs/folder? uri)            (-> {:status 200
+                                       :body   result}
+                                      (cc/cache-control uri))
+      (fs/does-not-exist? result) (not-found)
+      (fs/not-modified? result)   (not-modified)
+      :else                       (-> {:status  200
+                                       :headers {"ETag" (fs/object-version result)}
+                                       :body    (fs/object-content result)}
+                                      (cc/cache-control uri)))))
+
+(defn kebab-case-headers
+  [{:keys [headers] :as request}]
+  (cond-> request
+    headers (assoc :headers (cske/transform-keys csk/->kebab-case headers))))
+
+(defn set-host
+  [request host]
+  (assoc-in request [:headers "host"] host))
+
 (def index-routes
+  "All served from a common bucket: the Kaleidoscope app bucket."
   (context "/" []
     (GET "/" request
       :components [static-content-adapters]
-      (span/with-span! {:name (format "kaleidoscope.index.get")}
+      (span/with-span! {:name "kaleidoscope.index.get"}
         {:status  200
          :headers {"Content-Type" "text/html"}
          :body    (-> static-content-adapters
@@ -76,7 +104,7 @@
                       (fs/object-content))}))
     (GET "/index.html" request
       :components [static-content-adapters]
-      (span/with-span! {:name (format "kaleidoscope.index.get")}
+      (span/with-span! {:name "kaleidoscope.index.get"}
         {:status  200
          :headers {"Content-Type" "text/html"}
          :body    (-> static-content-adapters
@@ -85,36 +113,31 @@
                       (fs/object-content))}))
     (GET "/silent-check-sso.html" request
       :components [static-content-adapters]
-      (span/with-span! {:name (format "kaleidoscope.silent-check-sso.get")}
+      (span/with-span! {:name "kaleidoscope.silent-check-sso.get"}
         {:status  200
          :headers {"Content-Type" "text/html"}
          :body    (-> static-content-adapters
                       (get KALEIDOSCOPE)
                       (fs/get "silent-check-sso.html")
                       (fs/object-content))}))
-    ))
+
+    ;; Get frontend app from the Kaleidoscope bucket, so all sites don't need to
+    ;; have separate copies of the app (and all updates immediately apply to all
+    ;; sites).
+    (GET "/js/compiled/andrewslai/*" request
+      :components [static-content-adapters]
+      (let [uri (:uri request)]
+        (span/with-span! {:name (format "kaleidoscope.%s.get" (str/replace uri #"/" "."))}
+          (get-resource static-content-adapters (-> request
+                                                    kebab-case-headers
+                                                    (set-host "kaleidoscope.pub"))))))))
 
 (def default-handler
   (GET "*" {:keys [uri headers] :as request}
     :components [static-content-adapters]
     (span/with-span! {:name (format "kaleidoscope.default.handler.get")}
-      (let [request (cond-> request
-                      headers (assoc :headers (cske/transform-keys csk/->kebab-case headers)))
-            result  (-> static-content-adapters
-                        (get (bucket-name request))
-                        (fs/get uri (if-let [version (get-in request [:headers "if-none-match"])]
-                                      {:version version}
-                                      {})))]
-        (cond
-          (fs/folder? uri)            (-> {:status 200
-                                           :body   result}
-                                          (cc/cache-control uri))
-          (fs/does-not-exist? result) (not-found)
-          (fs/not-modified? result)   (not-modified)
-          :else                       (-> {:status  200
-                                           :headers {"ETag" (fs/object-version result)}
-                                           :body    (fs/object-content result)}
-                                          (cc/cache-control uri)))))))
+      (get-resource static-content-adapters (-> request
+                                                kebab-case-headers)))))
 
 (defn kaleidoscope-app
   [{:keys [http-mw] :as components}]
