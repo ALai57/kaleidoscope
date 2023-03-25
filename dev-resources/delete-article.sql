bf01b4dd8cd4a9420d5714774d61e94c60bfa2b7
Andrew Lai <andrew.s.lai5@gmail.com>
2023-03-25 17:32:05 -0500

    Documentation/helpers

---
+++ dev-resources/delete-article.sql
@@ -0,0 +1,8 @@
+
+SELECT * FROM article_branches WHERE article_id = ;
+
+DELETE FROM article_versions WHERE branch_id = ;
+
+DELETE FROM article_branches WHERE id = ;
+
+DELETE FROM articles WHERE id = ;
