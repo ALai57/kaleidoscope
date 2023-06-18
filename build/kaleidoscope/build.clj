f81996fcab4ad4aff7292a85f68a7df82aa81d75
Andrew Lai <andrew.s.lai5@gmail.com>
2023-06-17 23:02:59 -0500

    Add build steps

--- build/kaleidoscope/build.clj
+++ build/kaleidoscope/build.clj
@@ -1,5 +1,7 @@
 (ns kaleidoscope.build
-  (:require [clojure.tools.build.api :as b]))
+  (:refer-clojure :exclude [compile])
+  (:require [clojure.tools.build.api :as b]
+            [clojure.java.shell :as shell]))
 
 (def LIB 'org.clojars.alai57/kaleidoscope)
 (def MAIN (symbol (format "%s.main" (name LIB))))
@@ -7,34 +9,48 @@
 (def BASIS (b/create-basis {:project "deps.edn"}))
 
 ;; Directory structure
-(def OUTPUT-DIR "target2")
+(def OUTPUT-DIR "target")
 (def CLASS-DIR (format "%s/classes" OUTPUT-DIR))
 (def UBER-FILE (format "%s/%s.jar" OUTPUT-DIR (name LIB)))
+
+(defn git-status []
+  (shell/sh "git" "status"))
+
+(def nonzero?
+  (complement zero?))
+
+(defn assert-committed
+  [_]
+  (let [{:keys [exit out] :as result} (git-status)]
+    (if (or (nonzero? exit)
+            (re-find #"Changes (not staged for commit|to be committed)" out))
+      (throw (Exception. "Uncommitted changes in project."))
+      true)))
 
 (defn clean [_]
   (b/delete {:path OUTPUT-DIR}))
 
-(defn uberjar [_]
+(defn compile [_]
   (clean nil)
   (b/write-pom {:class-dir CLASS-DIR
                 :lib       LIB
                 :version   VERSION
                 :basis     BASIS
-                :src-dirs  ["src" "resources"]
-                })
+                :src-dirs  ["src" "resources"]})
   (b/copy-dir {:src-dirs   ["src" "resources"]
                :target-dir CLASS-DIR})
   (b/compile-clj {:basis        BASIS
                   :src-dirs     ["src"]
-                  :class-dir    CLASS-DIR})
+                  :class-dir    CLASS-DIR}))
+
+(defn uberjar [_]
+  (compile nil)
   (b/uber {:class-dir CLASS-DIR
            :uber-file UBER-FILE
            :basis     BASIS
            :main      MAIN}))
 
-(defn dockerize [_]
-  )
-
 (comment
   (uberjar nil)
+  (assert-committed)
   )
