6c9a6bea7ba9bd5e981cf9cc93a581a5275cde03
Andrew Lai <alai@Andrews-MacBook-Air.local>
2023-09-16 21:59:32 -0500

    Fix broken test

--- test/kaleidoscope/init/env_test.clj
+++ test/kaleidoscope/init/env_test.clj
@@ -1,8 +1,11 @@
 (ns kaleidoscope.init.env-test
   (:require [clojure.test :refer :all]
-            [kaleidoscope.init.env :as sut]))
+            [kaleidoscope.init.env :as sut]
+            [kaleidoscope.main :as main]
+            [malli.instrument :as mi]))
 
 (deftest env->pg-conn-throws-when-missing-env-vars
+  (main/initialize-schema-enforcement!)
   (is (thrown-with-msg? clojure.lang.ExceptionInfo
                         #"Missing DB name. Set via KALEIDOSCOPE_DB_NAME environment variable."
                         (sut/env->pg-conn {})))
@@ -11,4 +14,6 @@
                          (sut/env->pg-conn {"KALEIDOSCOPE_DB_NAME" "Hi"})
                          (throw (Exception. "Should blow up before this with ex-info"))
                          (catch clojure.lang.ExceptionInfo e
-                           (ex-message e)))))))
+                           (ex-message e))))))
+
+  (mi/unstrument!))
