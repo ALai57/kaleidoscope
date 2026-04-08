da18d475312a0057683a2c792ce1310f72c2f637
Andrew Lai <andrew.s.lai5@gmail.com>
2026-04-07 22:30:49 -0500

    Fly IO access to S3

--- iac/blog-sites/s3.tf
+++ iac/blog-sites/s3.tf
@@ -1,3 +1,77 @@
+##############################################################
+# IAM user for Fly.io → S3 access
+# (Fly has no IAM role support, so credentials are injected
+#  as secrets via `fly secrets set`)
+##############################################################
+
+resource "aws_iam_user" "kaleidoscope_fly" {
+  name = "kaleidoscope-fly"
+}
+
+resource "aws_iam_user_policy" "kaleidoscope_fly_s3" {
+  name = "kaleidoscope-s3"
+  user = aws_iam_user.kaleidoscope_fly.name
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:GetObject",
+          "s3:PutObject",
+          "s3:DeleteObject",
+          "s3:GetObjectTagging",
+          "s3:PutObjectTagging",
+          "s3:GetObjectVersion",
+          "s3:DeleteObjectVersion",
+          "s3:AbortMultipartUpload",
+        ]
+        Resource = [
+          "arn:aws:s3:::andrewslai.com/*",
+          "arn:aws:s3:::caheriaguilar.com/*",
+          "arn:aws:s3:::sahiltalkingcents.com/*",
+          "arn:aws:s3:::wedding/*",
+          "arn:aws:s3:::kaleidoscope.pub/*",
+          "arn:aws:s3:::kaleidoscope.client/*",
+        ]
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:ListBucket",
+          "s3:ListBucketVersions",
+        ]
+        Resource = [
+          "arn:aws:s3:::andrewslai.com",
+          "arn:aws:s3:::caheriaguilar.com",
+          "arn:aws:s3:::sahiltalkingcents.com",
+          "arn:aws:s3:::wedding",
+          "arn:aws:s3:::kaleidoscope.pub",
+          "arn:aws:s3:::kaleidoscope.client",
+        ]
+      },
+    ]
+  })
+}
+
+resource "aws_iam_access_key" "kaleidoscope_fly" {
+  user = aws_iam_user.kaleidoscope_fly.name
+}
+
+output "kaleidoscope_fly_access_key_id" {
+  value = aws_iam_access_key.kaleidoscope_fly.id
+}
+
+output "kaleidoscope_fly_secret_access_key" {
+  value     = aws_iam_access_key.kaleidoscope_fly.secret
+  sensitive = true
+}
+
+##############################################################
+# S3 buckets
+##############################################################
+
 resource "aws_s3_bucket" "blog_buckets" {
   for_each = toset(["andrewslai", "caheriaguilar", "sahiltalkingcents", "andrewslai.com", "caheriaguilar.com", "sahiltalkingcents.com"])
   bucket = each.key
