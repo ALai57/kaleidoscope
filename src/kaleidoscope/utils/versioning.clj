70c82687a71530b361b07fefe6273503ec8971ed
Andrew Lai <andrew.s.lai5@gmail.com>
2026-04-10 09:59:07 -0500

    Fix git dependency issue

--- src/kaleidoscope/utils/versioning.clj
+++ src/kaleidoscope/utils/versioning.clj
@@ -1,15 +1,14 @@
 (ns kaleidoscope.utils.versioning
-  (:require [clojure.java.shell :as shell]
-            [clojure.java.io :as io]
+  (:require [clojure.java.io :as io]
             [clojure.string :as string]
             [clojure.walk :as walk]))
 
-(def pom-path
-  "META-INF/maven/org.clojars.alai57/kaleidoscope/pom.properties")
+(def version-path
+  "version.properties")
 
-(defn parse-pom
-  [pom-path]
-  (some->> (io/resource pom-path)
+(defn parse-properties
+  [resource-path]
+  (some->> (io/resource resource-path)
            slurp
            (#(string/split % #"\n|="))
            (partition 2)
@@ -17,13 +16,6 @@
            (into {})
            walk/keywordize-keys))
 
-(defn short-sha
-  []
-  (-> (shell/sh "git" "rev-parse" "--short" "HEAD")
-      :out
-      string/trim))
-
 (defn get-version-details []
-  (or (parse-pom pom-path)
-      {:revision (short-sha)
-       :version  "version-not-set"}))
+  (or (parse-properties version-path)
+      {:version "unknown" :revision "unknown"}))
