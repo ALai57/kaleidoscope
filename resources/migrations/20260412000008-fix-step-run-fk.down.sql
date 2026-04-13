933ec8e6e169e182a7818092b77cd2232954e618
Andrew Lai <andrew.s.lai5@gmail.com>
2026-04-12 22:57:45 -0500

    Fix db issue

---
+++ resources/migrations/20260412000008-fix-step-run-fk.down.sql
@@ -0,0 +1,8 @@
+ALTER TABLE project_workflow_step_runs
+  DROP CONSTRAINT project_workflow_step_runs_step_id_fkey;
+
+--;;
+
+ALTER TABLE project_workflow_step_runs
+  ADD CONSTRAINT project_workflow_step_runs_step_id_fkey
+  FOREIGN KEY (step_id) REFERENCES workflow_steps(id);
